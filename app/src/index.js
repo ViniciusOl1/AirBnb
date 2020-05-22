1fe7fd2c7414e647ded7f434ca0b3935eccc209b
Vinicius Oliveira de Freitas <vinicius@iMac-de-Vinicius.local>
2020-05-22 13:52:58 -0300

    Colocando Mapa na tela

--- app/src/index.js
+++ app/src/index.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import Routes from './routes';
+import MapboxGL from '@mapbox/react-native-mapbox-gl';
+
+MapboxGL.setAccessToken('pk.eyJ1IjoidmluaWNpdXN2b2YiLCJhIjoiY2thaWZlaXF3MDA3NDJ0dGR5em92aWF1MyJ9.dgRwb_fi2IbIgfquYuwXnQ');
 
 const App = () => <Routes />;
 
